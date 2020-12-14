265bc153318fdaefcc62ae8053b77e25424cec93
Ya-dola <Ya-dola@users.noreply.github.com>
2020-12-14 12:46:56 +0300

    Added Destructables Breaking on Collision

---
+++ Assets/Scripts/DestructableController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableController : MonoBehaviour
+{
+    public GameObject DestBrokenPrefab;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        // To check if the player collided with the destructable
+        PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+
+        // TODO - Change it to destroy only when collides with the disc
+
+        if (!playerController)
+            return;
+
+        GameObject destBroken = Instantiate(DestBrokenPrefab, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+
+        Destroy(destBroken, GameManager.singleton.destBrokenDelay);
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager singleton;
+    public bool GameStarted { get; private set; }
+    public bool GameEnded { get; private set; }
+    public bool GamePaused { get; private set; }
+    // public bool GameWon { get; private set; }
+    // public bool GameLost { get; private set; }
+
+    [Header("Destructables")]
+    public float destBrokenDelay;
+
+    void Awake()
+    {
+        // Creates a Single Instance of the game manager through out the entire game
+        if (singleton == null)
+            singleton = this;
+        else if (singleton != this)
+            Destroy(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void StartGame()
+    {
+        GameStarted = true;
+    }
+
+    public void PauseOrResumeGame()
+    {
+        GamePaused = !GamePaused;
+
+        // pauseMenu.SetActive(GamePaused);
+
+        // if (GamePaused)
+        // {
+        //     Time.timeScale = 0f;
+
+        //     // Pauses Player Running Sound in the background
+        //     // playerController.playerRunningAudioSource.Pause();
+        // }
+        // else
+        // {
+        //     Time.timeScale = 1f;
+
+        //     // Plays Player Running Sound in the background
+        //     // playerController.playerRunningAudioSource.Play();
+        // }
+    }
+
+    public void EndGame(bool gameWon)
+    {
+        GameEnded = true;
     }
 }
